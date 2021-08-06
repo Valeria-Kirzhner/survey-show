4938e0b427c6b5fbc61863ab13940152872c22c1
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-06 16:32:24 +0300

    added a port - heroku port or 5000 for the local.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;// the port heroku is pretent to provide or 5000 for the local.
+app.listen(PORT);
