9750cd55025a9c66b112745fdbb70908939289cc
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-08 23:12:02 +0300

    passport library and google strategy library was installed and required in index.js .

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');// the core of the logic for authtication with some application.
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
 
 const PORT = process.env.PORT || 5000;// the port heroku is pretent to provide or 5000 for the local.
 app.listen(PORT);
