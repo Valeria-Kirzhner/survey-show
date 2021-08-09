4b810984efbe9296db1fd6f1fbe2513e64e80788
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-09 16:16:43 +0300

    added to google strategy some properties with google keys and callback where google should redirect the client. And the second property is arrow function accessToken.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const passport = require('passport');// the core of the logic for authtication with some application.
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use( new GoogleStrategy());
+passport.use( new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'    // where should google redirect the user next.
+}, 
+accessToken =>{
+    console.log(accessToken);
+}
+));
+
+
 
 const PORT = process.env.PORT || 5000;// the port heroku is pretent to provide or 5000 for the local.
 app.listen(PORT);
