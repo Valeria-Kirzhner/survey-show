4f44b290f353ebf950823f228d23721b3354eb80
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-09 15:19:35 +0300

    added client id and secret key for google api oauth .

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
 const passport = require('passport');// the core of the logic for authtication with some application.
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const app = express();
+
+passport.use( new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;// the port heroku is pretent to provide or 5000 for the local.
 app.listen(PORT);
