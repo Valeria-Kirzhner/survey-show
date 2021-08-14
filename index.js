b0724438b6d14c471abcc3065d8092e2c74d0dfa
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-14 17:58:24 +0300

    createt conection to the database by the mongoose. the mongoUri is hidden in secret in different file.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 const app = express();
 require('./routes/auth')(app); // authRoutes return an arrow function . The (app) is an argument that immidiately calls the module.export function that wrap the rautes and they need the conection with express.
 
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;// the port heroku is pretent to provide or 5000 for the local.
 app.listen(PORT);
