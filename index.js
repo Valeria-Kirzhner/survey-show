340cc01be9b7dd4bb17a23c55688c206b0eb59a9
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-16 13:36:15 +0300

    added to passport configuration into the callback to create new User using the model User and save it in the db. For now user file will include only googleid.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User'); 
 require('./services/passport');
-const app = express();
-require('./routes/auth')(app); // authRoutes return an arrow function . The (app) is an argument that immidiately calls the module.export function that wrap the rautes and they need the conection with express.
-require('./models/User');
 
 mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+require('./routes/auth')(app); // authRoutes return an arrow function . The (app) is an argument that immidiately calls the module.export function that wrap the rautes and they need the conection with express.
 
 const PORT = process.env.PORT || 5000;// the port heroku is pretent to provide or 5000 for the local.
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');// the core of the logic for authtication with some application.
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 // passport configuration
 passport.use( new GoogleStrategy({ // google strategy response differently in use of the two requests (first and second) becouse it checks if the CODE is attached or not.
@@ -9,8 +12,8 @@ passport.use( new GoogleStrategy({ // google strategy response differently in us
     callbackURL: '/auth/google/callback'    // where should google redirect the user next.
 }, 
 (accessToken, refreshToken, profile, done) => {
-    console.log('Access Token', accessToken);
-    console.log('Refresh Token', refreshToken);
-    console.log('Profile : ', profile);
+
+    new User({ googleId: profile.id  }).save(); 
+
 }
 ));
