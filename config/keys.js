2258512730ee7f1ec66ee6b556d73a05e5787120
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-18 17:11:48 +0300

    keys.js will check if the app is running on environment or on the development, and by checkig will chouse which keys to use.

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+// figure out what set of credentials to return
+
+if ( process.env.NODE_ENV === 'production' ) {  //  if in production - return production set of keys
+
+    module.exports = require('./prod');
+     
+} else {// we are in development - return the dev keys
+
+    module.exports = require('./dev');
+
+}
