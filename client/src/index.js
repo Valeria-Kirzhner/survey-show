9c99911735f550afd7dfcf0f803f7d6ccf87bf26
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-21 13:46:42 +0300

    imported App.js component and pused to be rendered in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
 
-ReactDOM.render();// ReactDom render the root component that handle all the components. 
+import App from './components/App'; 
+
+ReactDOM.render(<App />, document.querySelector('#root'));// ReactDom render the root component that handle all the components. The '#root' you can found in public/index.html
