f42955117d33021c5fbbe61bb4435a462973d56e
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-21 22:10:29 +0300

    added a function into fetchUser and it send a get request with axios to api/current_user. Then when will be response it would be dispatched.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { FETCH_USER } from './types'; 
 
-//action creator
+//action creator - his purpouse is to return an action and then the dispatch sent it to different reducers inside the store. It produces a new value for state and updetes the state inside of the store. And then the store updates all the components .
 const fetchUser = () => {
 
+    return function(dispatch){
+
+         axios.get('api/current_user')
+         .then( res => dispatch({ type: FETCH_USER, payload: res  }))
+
+    }
      
 };
