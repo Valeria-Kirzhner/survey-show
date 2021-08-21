20b232fe72885751102c9292c3443aa7129b0b32
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-21 17:37:06 +0300

    created almost empty function for fundomental authReducer for hook it up in index.js inside combineReducers function

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,9 @@
+export default function (state = {}, action) {// for now = {} becouse it need to be returned and i don't wan't it to be undifiend
+    
+    switch (action.type) {
+
+        default:
+            return state;
+
+     }
+}
