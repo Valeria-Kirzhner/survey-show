d1fb1e7a8bcb46fe7ba354889daeba90bb3cb086
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-21 15:04:29 +0300

    made some first redux setups  in index.js and a store created

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App'; 
 
-ReactDOM.render(<App />, document.querySelector('#root'));// ReactDom render the root component that handle all the components. The '#root' you can found in public/index.html
+const store = createStore(()=> [] , {}, applyMiddleware()); // the 1 argument is all the reducers, the 2 argument is the starting/initial state of the app - this more for server side rendering which is not in my case,  
+
+ReactDOM.render(
+< Provider store={store}> < App /> </Provider>,
+document.querySelector('#root')); // ReactDom render the root component that handle all the components. The '#root' you can found in public/index.html
