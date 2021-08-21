948a81470dc0a287adc6c398e8225e8ccd94bbe5
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-21 21:19:43 +0300

    reduxThunk was imported and passed by first argument in applyMiddleware in the store

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 
 
 import App from './components/App'; 
 import reducers from './reducers';
 
-const store = createStore( reducers , {}, applyMiddleware()); // the 1 argument is all the reducers, the 2 argument is the starting/initial state of the app - this more for server side rendering which is not in my case,  
+const store = createStore( reducers , {}, applyMiddleware(reduxThunk)); // the 1 argument is all the reducers, the 2 argument is the starting/initial state of the app - this more for server side rendering which is not in my case,  
 
 ReactDOM.render(// ReactDom render the root component that handle all the components. The '#root' you can find in public/index.html
 
