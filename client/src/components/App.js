0eb23a7e6686e33459b3ba184d9eb7196db3b01f
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-21 22:43:28 +0300

    connect function from react redux was imported in App.js sticked to the default export app so it made a connection between all the actions .

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route} from 'react-router-dom'; 
+import { connect } from "react-redux";// it's a tool to connect between the hook - on did mount and redux store.
+import * as actions from '../actions';
 
 import Header from '../components/Header';
 const Dashboard = () => <h2>Dashboard</h2>;
@@ -9,7 +11,11 @@ const Landing = () => <h2>Landing</h2>;
 
 class App extends Component{
 
-    render(){
+    componentDidMount () {
+
+    }
+
+    render () {
 
     return(
 
@@ -25,8 +31,8 @@ class App extends Component{
             </BrowserRouter>
         </div>
 
-     ); 
+     );  
    };
 };
-
-export default App;
+// coonect is a tool to connect between the hook - on did mount and redux store.
+export default connect(null, actions ) (App);// The first argument of connect, is reserved for the map state to prop's arguments which i woudn't use. And the second arg is * imported actions.
