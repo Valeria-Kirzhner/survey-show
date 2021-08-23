b155c27ef434f136ee53ef97507ef52390a2cdda
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-23 19:38:34 +0300

    the state in authReducer will be by default null = don't know if the user is loged in till he get the response, and added case FETCH_USER that can be returned true or false.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,8 +5,8 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => async dispatch => {// arrow func inside arrow func 
 
    const res = await axios.get('api/current_user');
-   
-    dispatch({ type: FETCH_USER, payload: res  });
+
+    dispatch({ type: FETCH_USER, payload: res.data  });
 
 };
      
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,14 @@
-export default function (state = {}, action) {// for now = {} becouse it need to be returned and i don't wan't it to be undifiend
-    
+import { FETCH_USER } from "../actions/types";
+
+const authReducer = function (state = null, action) {// for now = {} becouse it need to be returned and i don't wan't it to be undifiend
     switch (action.type) {
+
+        case FETCH_USER:
+            return action.payload || false ; // the user model = true || false = the user loged out. the state by deafult return null becouse I can't know if the user is logged in till I recieve the response from the get req. to api/current_user
 
         default:
             return state;
 
      }
-}
+};
+export default authReducer;
