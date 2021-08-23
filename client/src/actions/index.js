7370e0a0bc7de580ce43439b06052bb9e9280263
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-23 19:05:33 +0300

    the function fetchUser cleaned from over code

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,13 +2,11 @@ import axios from 'axios';
 import { FETCH_USER } from './types'; 
 
 //action creator - his purpouse is to return an action and then the dispatch sent it to different reducers inside the store. It produces a new value for state and updetes the state inside of the store. And then the store updates all the components .
-const fetchUser = () => {
+export const fetchUser = () => async dispatch => {// arrow func inside arrow func 
 
-    return function(dispatch){
+   const res = await axios.get('api/current_user');
+   
+    dispatch({ type: FETCH_USER, payload: res  });
 
-         axios.get('api/current_user')
-         .then( res => dispatch({ type: FETCH_USER, payload: res  }))
-
-    }
-     
 };
+     
